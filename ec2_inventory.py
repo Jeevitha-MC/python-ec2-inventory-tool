12cac185f72e2dc6c3a5fd90ef23f9a29d25ae2b
Jeevitha-MC <115450377+Jeevitha-MC@users.noreply.github.com>
2026-07-09 17:28:13 +0530

    Remove argparse for EC2 inventory tool
    Removed argparse for command-line argument parsing.

--- ec2_inventory.py
+++ ec2_inventory.py
@@ -1,7 +1,6 @@
 #Import response variable from sample_data.py
 import boto3
 import csv
-import argparse
 
 #Create an EC2 client
 ec2 = boto3.client("ec2")
@@ -12,12 +11,6 @@ response = ec2.describe_instances()
 
 print(type(response))
 print(response.keys())
-
-parser = argparse.ArgumentParser(description="EC2 Inventory Tool")
-parser.add_argument(
-    "--state",
-    help="Filter instances by state (running/stopped)"
-)
 
 def get_all_instances(response):
     #Store all instances in a list
